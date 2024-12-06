110ff274b6828a8120355387dad3eaed1827e95a
anlol12 <andersonberrios105@gmail.com>
2024-12-05 22:38:40 -0600

    render6

--- TecnoFusion3/build.sh
+++ TecnoFusion3/build.sh
@@ -2,7 +2,11 @@
 
 set -o errexit
 
+# Instalar las dependencias desde requirements.txt en el directorio TecnoFusion3
 pip install -r TecnoFusion3/requirements.txt
 
-python manage.py collectstatic --noinput
+# Ejecutar collectstatic desde el archivo manage.py dentro de TecnoFusion3
+python TecnoFusion3/manage.py collectstatic --noinput
+
+# Aplicar migraciones desde el archivo manage.py dentro de TecnoFusion3
 python TecnoFusion3/manage.py migrate
